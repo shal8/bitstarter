bc4009f390aa5975b2e64a313bcbd04ff03572c3
shal8 <snp3506@yahoo.com>
2013-07-15 17:03:34 -0700

    Update web.js

--- web.js
+++ web.js
@@ -2,8 +2,19 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var read_html = function(filename){
+  var fs = require('fs');
+	var file = fs.readFileSync(filename);
+	console.log("file.length " + file.length);
+	console.log("file " + file);
+	var buffer = new Buffer(file.length);
+	buffer.write(file.toString(), "utf-8")
+	console.log("buffer.toString " + buffer.toString());
+	return buffer.toString();
+}
+
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  response.send(read_html("index.htm"));
 });
 
 var port = process.env.PORT || 5000;
